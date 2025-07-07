f7c459adc3b78db23d4c2c85685bb91dcea1956a
Thayc Marinho <thaycmarinho@hotmail.com.br>
2025-07-07 17:34:05 +0100

    Implementation of extraction in qi module data focused in complaints extraction

--- extract_iqs_actions.py
+++ extract_iqs_actions.py
@@ -16,8 +16,17 @@ mm_module_icon1=BASE_DIR/"icons"/"MM-module-icon.png"
 employee_icon1=BASE_DIR/"icons"/"employee-icon.png"
 employee_icon2=BASE_DIR/"icons"/"mm-module-icon2.png"
 export_option=BASE_DIR/"icons"/"export-options.png"
-
-
+qi_icon_w=BASE_DIR/"icons"/"qi_icon_w.png"
+qi_icon_g=BASE_DIR/"icons"/"qi_icon_g.png"
+reports_icon=BASE_DIR/"icons"/"reports.png"
+analysis_icon=BASE_DIR/"icons"/"show_a.png"
+search_icon=BASE_DIR/"icons"/"search_icon.png"
+complaint_icon=BASE_DIR/"icons"/"complaints_year.png"
+confirm_complaint= BASE_DIR/"icons"/"complaints_opened.png"
+working_list_icon= BASE_DIR/"icons"/"working_list.png"
+update_icon= BASE_DIR/"icons"/"update.png"
+options_icon= BASE_DIR/"icons"/"options_icons.png"
+excel_icon=BASE_DIR/"icons"/"excel_icon.png"
 
 pa.PAUSE=0.5
 pa.FAILSAFE=True
@@ -127,11 +136,103 @@ def export_table():
     finish_bt=(1109,772)
     pa.moveTo(finish_bt)
     pa.click()
+    
+
+def qi_module():
+    module_icon=pa.locateOnScreen(str(modules_icons),confidence=0.9)
+    pa.doubleClick(module_icon)
+    pa.click(module_icon)
+    try:
+        qi_mm= wait_image(str(qi_icon_w))
+    except ImageNotFoundException:
+        qi_mm=wait_image(str(qi_icon_g))
+    pa.doubleClick(qi_mm)
+    try:
+        wait_image(str(reports_icon))
+    except ImageNotFoundException:
+        qi_module()
+    try:
+        analysis=wait_image(str(analysis_icon))
+    except ImageNotFoundException:
+        pass
+    pa.click(analysis)
+    try:
+        search=wait_image(str(search_icon))
+    except ImageNotFoundException:
+        search= (36,202)
+        print("Posição localizada pelas coordenadas.")
+    pa.click(search)
+    pa.write("Complaints per year and method")
+    pa.press("Enter")
+    try:
+        complaint=wait_image(str(complaint_icon))
+    except ImageNotFoundException:
+        pass
+    pa.doubleClick(complaint)
+    try:
+        wait_image(str(confirm_complaint))
+    except:
+        pass
+    try:
+        working_list=wait_image(str(working_list_icon))
+    except:
+        pass
+    pa.doubleClick(working_list)
+    pa.press("Enter")
+    sleep(2)
+    update= wait_image(str(update_icon),confidence=0.9)
+    pa.click(update)
+    pa.click(1463,444)
+    try:
+        options=wait_image(str(options_icon))
+    except ImageNotFoundException:
+        pa.click(1463,444)
+        try:
+            options=wait_image(str(options_icon))
+        except ImageNotFoundException:
+            print("icone de options não encontrado.")
+    pa.click(options)
+    excel=wait_image(str(excel_icon))
+    pa.click(excel)
+####################################################################################
+    next_1=(1013,773)
+    pa.moveTo(next_1)
+    pa.click()
+    file_options=pa.locateOnScreen(str(export_option))
+    if file_options:
+        pa.moveTo(file_options)
+        pa.click()
+    for _ in range(6):
+        pa.press('tab')
+    pa.press('enter')
+    pa.write(r"C:/Users/00071228/OneDrive - ENERCON/QA Team - Follow up - Qualidade - databases")
+    pa.press("enter")
+    for _ in range(7):
+        pa.press('tab')
+    db_name='problems_db'
+    pa.write(db_name)
+    pa.press('Enter')
+    pa.press('left')
+    pa.press('Enter')
+    pa.moveTo(next_1)
+    pa.click()
+    finish_opt=(772,409)
+    pa.moveTo(finish_opt)
+    pa.click()
+    finish_bt=(1109,772)
+    pa.moveTo(finish_bt)
+    pa.click()
+            
+def close_iqs():
     pa.click(1665,12,2,1)
+    
+
 def main():
     step1=open_iqs()
     open_mm_module(step1)
     export_table()
+    qi_module()
+    close_iqs()
 
 if __name__=="__main__":
     main()
